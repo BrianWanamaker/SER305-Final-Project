c9928fd556f2dce198e5777dcfee33aee21de722
kevin-rodri <kr399880@gmail.com>
2024-04-15 16:19:15 -0400

    this will break the master branch

--- script.js
+++ script.js
@@ -5,13 +5,67 @@ let image2 = new Image();
 let canvas = document.getElementById("imageCanvas");
 let ctx = canvas.getContext("2d");
 
-function handleImageUpload(event) {
+// async function handleImageUpload(event) {
+//   let reader = new FileReader();
+//   let img = new Image();
+//   let targetId = event.target.id;
+
+ 
+
+//   reader.onload = function (e) {
+//     img.onload = async () => {
+//       if (targetId === "selectImage1") {
+//         image1.src = e.target.result;
+//         image1Loaded = true;
+//       } else if (targetId === "selectImage2") {
+//         image2.src = e.target.result;
+//         image2Loaded = true;
+//       }
+//       if (image1Loaded && image2Loaded) {
+//         canvas.width = image1.width;
+//         canvas.height = image1.height;
+//         updateFade(0);
+//       }
+
+//       if (image1.width != image2.width || image1.height != image2.height){
+//         const url = `https://image-editor1.p.rapidapi.com/api/image/resize?width=${image1.width}&height=${image1.height}`;
+//         const data = new FormData();
+//         console.log(image2.src);
+//         data.append('image', image2.src);
+        
+        
+//               const options = {
+//                 method: 'POST',
+//                 headers: {
+//                   Accept: 'image/*', // this can be modified
+//                   'X-RapidAPI-Key': '116addc457msh4580296aaa39d98p191d68jsn904ccd158204',
+//                   'X-RapidAPI-Host': 'image-editor1.p.rapidapi.com'
+//                 },
+//                 body: data
+//               };
+        
+//               try {
+//                 const response = await fetch(url, options);
+//                 const result = await data;
+//                 console.log("Result" + result);
+//                 return result; 
+//               } catch (error) {
+//                 console.error(error);
+//               }
+//       }
+//     };
+//     img.src = e.target.result;
+//   };
+//   reader.readAsDataURL(event.target.files[0]);
+// }
+
+async function handleImageUpload(event) {
   let reader = new FileReader();
   let img = new Image();
   let targetId = event.target.id;
 
-  reader.onload = function (e) {
-    img.onload = () => {
+  reader.onload = async function (e) {
+    img.onload = async () => {
       if (targetId === "selectImage1") {
         image1.src = e.target.result;
         image1Loaded = true;
@@ -19,16 +73,53 @@ function handleImageUpload(event) {
         image2.src = e.target.result;
         image2Loaded = true;
       }
+
       if (image1Loaded && image2Loaded) {
         canvas.width = image1.width;
         canvas.height = image1.height;
-        updateFade(0);
+        
+        if (image1.width !== image2.width || image1.height !== image2.height) {
+          const url = `https://image-editor1.p.rapidapi.com/api/image/resize?width=${image1.width}&height=${image1.height}`;
+          const data = new FormData();
+          data.append('image', image2.src);
+
+          const options = {
+            method: 'POST',
+            headers: {
+              Accept: 'image/*',
+              'X-RapidAPI-Key': '116addc457msh4580296aaa39d98p191d68jsn904ccd158204',
+              'X-RapidAPI-Host': 'image-editor1.p.rapidapi.com'
+            },
+            body: data
+          };
+
+          try {
+            const response = await fetch(url, options);
+            const blob = await response.blob();
+            const resizedImageUrl = URL.createObjectURL(blob);
+
+            image2.onload = function() {
+              ctx.drawImage(image1, 0, 0, image1.width, image1.height);
+              ctx.drawImage(image2, 0, 0, image2.width, image2.height);
+              updateFade(0);
+            };
+            image2.src = resizedImageUrl;
+          } catch (error) {
+            console.error(error);
+          }
+        } else {
+          // If no resize is needed, proceed with the regular blending
+          ctx.drawImage(image1, 0, 0, image1.width, image1.height);
+          ctx.drawImage(image2, 0, 0, image2.width, image2.height);
+          updateFade(0);
+        }
       }
     };
     img.src = e.target.result;
   };
   reader.readAsDataURL(event.target.files[0]);
 }
+
 
 function fadeInto(sliderPosition) {
   if (!image1Loaded || !image2Loaded) return;
